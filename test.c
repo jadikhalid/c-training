4d57135d03208f6de9804f1af5b3c17199a8cfb8
Jadi Khalid <jadikhalid@gmail.com>
2026-03-11 09:42:00 +0000

    ok

--- test.c
+++ test.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-  printf("========================================\n");
-  printf("   Test du Compilateur GCC sur Linux    \n");
-  printf("========================================\n");
-
-  // Test de l'affichage simple
-  printf("[OK] Le binaire a ete genere avec succes.\n");
-
-  // Test des arguments (pour vérifier la liaison future)
-  if (argc > 1)
-  {
-    printf("[INFO] Argument detecte : %s\n", argv[1]);
-  }
-  else
-  {
-    printf("[INFO] Aucun argument passe au programme.\n");
-  }
-
-  printf("========================================\n");
+  printf("Hello, World!\n");
   return 0;
 }
