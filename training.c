5a2ded3850bfca8a6288e47ede21bc5f13a2dfca
jadikhalid <jadikhalid@gmail.com>
2026-03-23 10:56:11 +0100

    ok

--- training.c
+++ training.c
@@ -1,39 +1,24 @@
-/*Programme : trouver_nombre.c
-* Objectif : Ce programme choisit un nombre de facon aléatoire
-*            et demmade l'utilisateur de le trouver
-* Retour : aucun
+/*
+* Exemple de plusieurs instructions return
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define NO 0
-#define YES (!NO)
+int x,y,z;
+int larger_of(int a, int b);
 
-int main(void) {
-  int guess_Value = -1;
-  int nombre;
-  int nbr_of_gueses;
-  int done = NO;
+int main () {
+  puts("Entrez deux valeurs entieres diiférentes : ");
+  scanf("%d%d", &x, &y);
 
-  printf("Selection d'un nombre aleatoire\n");
-  srand(time(NULL));
-  nombre = rand();
+  z=larger_of(x,y);
 
-  nbr_of_gueses = 0;
-  while (done == NO) {
-    printf("\nDonnez un nombre entre 0 et %d\n", RAND_MAX);
-    scanf("%d", &guess_Value);
-    nbr_of_gueses++;
-    if(nombre == guess_Value) {
-      done = YES;
-    }else {
-      nombre<=guess_Value ? printf("Trop grand\n") : printf("Trop petit\n");
-    }
+  printf("\nLa valeur la plus grande est %d.", z);
+  return 0;
 }
 
-printf("\nVous avez trouve en %d essais\n", nbr_of_gueses);
-printf("Le nombre etait %d\n", nombre);
-return 0;
+int larger_of(int a, int b) {
+  if (a>b) return a;
+  else return b;
 }
