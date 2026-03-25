587e742c7ca71a7e353a67048234d769c293aa42
Jadi Khalid <jadikhalid@gmail.com>
2026-03-25 09:59:43 +0100

    Ok

--- training.c
+++ training.c
@@ -1,38 +1,19 @@
+/*
+ * Exemple de deux instruction for imbriquées
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 
-// Fonction récursive pour calculer 3^n
-long puissanceTrois(int n)
-{
-  // Cas de base : tout nombre à la puissance 0 vaut 1
-  if (n == 0)
-  {
-    return 1;
-  }
-  // Cas de base pour les puissances négatives (optionnel mais propre)
-  if (n < 0)
-  {
-    printf("Erreur : Ce programme ne gere que les exposants positifs.\n");
-    return 0;
-  }
-
-  // Cas récursif : 3^n = 3 * 3^(n-1)
-  return 3 * puissanceTrois(n - 1);
-}
+int count;
 
 int main()
 {
-  int exposant;
-  long resultat;
-
-  printf("Entrez l'exposant pour 3 (ex: 4 pour 3^4) : ");
-  scanf("%d", &exposant);
-
-  resultat = puissanceTrois(exposant);
-
-  if (exposant >= 0)
+  count = 1;
+  while (count <= 20)
   {
-    printf("3 a la puissance %d est egal a %ld\n", exposant, resultat);
+    printf("%d\n", count);
+    count++;
   }
-
   return 0;
 }
