647da6090d85cfb86003ee263399c6a59d30613b
jadikhalid <jadikhalid@gmail.com>
2026-03-29 12:24:29 +0100

    ok

--- training.c
+++ training.c
@@ -1,34 +1,25 @@
 /*
-* Tableau a 1000 valeurs
+* 
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 
-int random[10][10][10];
-int a,b,c;
+int tab[12][10];
 
 int main() {
-  for (a=0; a<10; a++) {
-    for (b=0; b<10; b++) {
-      for (c=0; c<10; c++) {
-        random[a][b][c] = rand();
-      }
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 10; j++) {
+            tab[i][j] = rand();
+        }
     }
-  }
 
-  for (a=0; a<10; a++) {
-    for (b=0; b<10; b++) {
-      for (c=0; c<10; c++) {
-        printf("[%d][%d][%d] = ", a, b, c);
-        printf("%d\n", random[a][b][c]);
-      }
-      printf("\nAppuyez sur Entrée pour continuer et CRTL-C pour sortir.");
-      getchar();
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tab[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 
-
-  return 0;
-  }
+    return 0;
+}
