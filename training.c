43077e7585dc09a0f10cb27f82a6dd6055935ee8
jadikhalid <jadikhalid@gmail.com>
2026-03-31 09:55:09 +0100

    ok

--- training.c
+++ training.c
@@ -1,34 +1,14 @@
 /*
-* passer un tableau a une fonction
-* en tant qu'argument : pointeur+taille
+* Nature numérique des variables char
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 10
+unsigned char x;
 
-int tab[MAX], count;
-
-int largest(int x[], int y);
-
-int main () {
-    for (count=0; count<MAX; count++) {
-        printf("Entrez un entier : ");
-        scanf("%d", &tab[count]);}
-
-        printf("\n\nLa valeur la lus grande est : %d\n ", largest(tab, MAX));
-
-        return 0;
-    
-}
-
-int largest(int x[], int y) {
-    int count, biggest=x[0];
-
-    for(count=1; count < y; count++)
-        if (x[count] > biggest)
-            biggest = x[count];
-    return biggest;
-
+int main() {
+   for (x=180; x<204; x++) {
+    printf("%c = %d\n", x, x);
+   }
 }
