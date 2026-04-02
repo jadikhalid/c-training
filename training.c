2568b2b5285cb315300a03052530f09b05e4e724
jadikhalid <jadikhalid@gmail.com>
2026-04-02 09:27:52 +0100

    ok

--- training.c
+++ training.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char input[81];
+char lmom[81], fnom[81];
+int count, id_num;
 
 int main() {
-   puts("Saisissez votre texte, puis qppuyez uer ENtree");
-   fgets(input, sizeof(input), stdin);
-   printf("Vous avez saisi : %s\n", input);
+   puts("Entrez votre nom, prenom et matricule separes");
+   puts("par des espaces et appuyez sur entrée");
 
-   exit(EXIT_SUCCESS);
+   count = scanf("%80s%80s%d", lmom, fnom, &id_num);
+
+   printf("%d", count);
+
+   return 0;
 }
