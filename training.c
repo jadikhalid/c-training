1d6e62625e1af223e2c4c2917d069e16178b8ba1
jadikhalid <jadikhalid@gmail.com>
2026-04-12 13:21:09 +0100

    ok

--- training.c
+++ training.c
@@ -1,67 +1,20 @@
-/*
- * xemple typique d'utilisation d'une union
- */
-
 #include <stdio.h>
-#include <stdlib.h>
-
-#define CARACTERE 'C'
-#define INTEGER 'I'
-#define FLOAT 'F'
-
-struct generic_tag
+struct data
 {
-  /* data */
-  char type;
-  union shared_tag
-  {
-    /* data */
-    char c;
-    int i;
-    float f;
-  } shared;
+  int membre1;
+  float membre2, membre3;
 };
 
-void print_fonction(struct generic_tag generic);
+struct data info = {
+    100,
+    2,
+    3.6};
+
+struct data *ptr = &info;
 
 int main()
 {
-  struct generic_tag var;
-
-  var.type = CARACTERE;
-  var.shared.c = '$';
-  print_fonction(var);
-
-  var.type = FLOAT;
-  var.shared.f = (float)12345.67890;
-  print_fonction(var);
-
-  var.type = 'x';
-  var.shared.i = 111;
-  print_fonction(var);
-
+  (*ptr).membre1 = 10;
+  printf("%d", (*ptr).membre1);
   return 0;
-}
-
-void print_fonction(struct generic_tag generic)
-{
-  printf("\nLa valeur generique est ...");
-  switch (generic.type)
-  {
-  case CARACTERE:
-    printf("%c", generic.shared.c);
-    break;
-
-  case INTEGER:
-    printf("%d", generic.shared.i);
-    break;
-
-  case FLOAT:
-    printf("%f", generic.shared.f);
-    break;
-
-  default:
-    printf("de type inconnu : %c\n", generic.type);
-    break;
-  }
 }
