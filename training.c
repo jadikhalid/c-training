55687912d5f5ad93a0478970feeaaadc33956b9e
jadikhalid <jadikhalid@gmail.com>
2026-04-22 09:47:23 +0100

    ok

--- training.c
+++ training.c
@@ -1,66 +1,18 @@
 /*
- * Réalisation d'un menu systeme avec une boucle infinie
+ * Illustration de la fonction system()
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
-int menu(void);
+#include "utilitaires.h"
 
 int main()
 {
-  int choix;
+  char input[40];
 
   while (1)
   {
-    choix = menu();
-    if (choix == 1)
-    {
-      puts("\nExécution de la tache correspondant au choix 1.");
-      sleep(5);
-    }
-    else if (choix == 2)
-    {
-      puts("\nExécution de la tache correspondant au choix 2.");
-      sleep(5);
-    }
-    else if (choix == 3)
-    {
-      puts("\nExécution de la tache correspondant au choix 3.");
-      sleep(5);
-    }
-    else if (choix == 4)
-    {
-      puts("\nExécution de la tache correspondant au choix 4.");
-      sleep(5);
-    }
-    else if (choix == 5)
-    {
-      puts("\nSortie du programme...\n");
-      sleep(5);
-      break;
-    }
-    else
-    {
-      puts("\nChoix incorrect, essayer de nouveau.");
-      sleep(5);
-    }
+    puts("\nEntrez une commande systeme, ou une ligne blanche pour sortir");
+    lire_clavier(input, sizeof(input));
   }
-  return 0;
-}
-
-int menu(void)
-{
-  int reponse;
-
-  puts("\nEntrez 1 pour la teche A.");
-  puts("\nEntrez 2 pour la teche B.");
-  puts("\nEntrez 3 pour la teche C.");
-  puts("\nEntrez 4 pour la teche D.");
-  puts("\nEntrez 5 pour sortir du programme.");
-
-  scanf("%d", &reponse);
-
-  return reponse;
 }
