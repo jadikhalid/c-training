fa5430f76c35a19cecb4075c88423dac93628e23
jadikhalid <jadikhalid@gmail.com>
2026-04-24 09:46:34 +0100

    ok

--- training.c
+++ training.c
@@ -1,56 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Fonction utilitaire pour vider le tampon d'entrée standard (stdin)
+void viderTampon()
+{
+  int c;
+  // Lit les caractères un par un jusqu'à trouver '\n' (Entrée) ou la fin de fichier (EOF)
+  while ((c = getchar()) != '\n' && c != EOF)
+  {
+    // On ne fait rien, on jette juste le caractère lu
+  }
+}
+
 int main()
 {
-  double num1, num2, resultat;
-  char operateur;
+  int choix;
 
-  printf("--- Calculatrice C v1.0 ---\n");
-  printf("Entrez l'operation (ex: 5 + 3) : ");
-
-  // Lecture : le premier nombre, l'operateur et le deuxieme nombre
-  // L'espace avant %c est crucial pour ignorer les espaces blancs
-  if (scanf("%lf %c %lf", &num1, &operateur, &num2) != 3)
+  do
   {
-    printf("Erreur : Format de saisie invalide.\n");
-    return 1;
-  }
+    // 1. Afficher le menu à chaque tour
+    printf("\n--- MENU DE COMMANDES SYSTEME ---\n");
+    printf("1. Lister les fichiers (dir / ls)\n");
+    printf("2. Creer un fichier texte (echo > ...)\n");
+    printf("3. Afficher la date et l'heure\n");
+    printf("4. Afficher les infos systeme\n");
+    printf("5. Quitter le programme\n");
+    printf("Votre choix : ");
 
-  switch (operateur)
-  {
-  case '+':
-    resultat = num1 + num2;
-    printf("Resultat : %.2f\n", resultat);
-    break;
-
-  case '-':
-    resultat = num1 - num2;
-    printf("Resultat : %.2f\n", resultat);
-    break;
-
-  case '*':
-    resultat = num1 * num2;
-    printf("Resultat : %.2f\n", resultat);
-    break;
-
-  case '/':
-    // Verification de la division par zero
-    if (num2 != 0)
+    // 2. Lire le choix de l'utilisateur
+    // On tente de lire un entier
+    if (scanf("%d", &choix) != 1)
     {
-      resultat = num1 / num2;
-      printf("Resultat : %.2f\n", resultat);
+      // SI CA ECHOUE (l'utilisateur a tapé du texte, 'a', 'bonjour'...)
+      printf("\n--- Erreur de saisie : Veuillez entrer un NOMBRE entier. ---\n");
+
+      // CORRECTION ICI : On vide le tampon pour enlever les caractères invalides
+      viderTampon();
+
+      // On affecte une valeur qui forcera le cas 'default' du switch
+      choix = 0;
     }
     else
     {
-      printf("Erreur : Division par zero impossible !\n");
+      // SI LA LECTURE DU NOMBRE A REUSSI
+      // Il reste souvent le '\n' (Entrée) dans le tampon.
+      // C'est une bonne habitude de vider le tampon même après un succès
+      // pour éviter des problèmes lors de futures lectures de caractères/chaînes.
+      viderTampon();
     }
-    break;
 
-  default:
-    printf("Erreur : Operateur '%c' non reconnu.\n", operateur);
-    break;
-  }
+    // 3. Agir en fonction du choix
+    // printf("\nAction choisie : %d\n", choix); // Optionnel, pour débugger
+
+    switch (choix)
+    {
+    case 1:
+      printf("\n--- Execution de 'lister fichiers' ---\n");
+#ifdef _WIN32
+      system("dir");
+#else
+      system("ls");
+#endif
+      break;
+    case 2:
+      printf("\n--- Creation de salut.txt ---\n");
+      system("echo Bonjour tout le monde > salut.txt");
+      break;
+    case 3:
+      printf("\n--- Affichage date/heure ---\n");
+#ifdef _WIN32
+      system("time /t & date /t");
+#else
+      system("date");
+#endif
+      break;
+    case 4:
+      printf("\n--- Infos Systeme ---\n");
+#ifdef _WIN32
+      system("systeminfo");
+#else
+      system("uname -a");
+#endif
+      break;
+    case 5:
+      printf("\nFin du programme. Au revoir !\n");
+      break;
+    default:
+      // Ce cas est atteint si choix == 0 (erreur de saisie)
+      // ou si l'utilisateur a tapé un nombre hors de 1-5 (ex: 8)
+      if (choix != 0)
+      {
+        printf("\n--- Choix invalide : Le nombre doit etre entre 1 et 5. ---\n");
+      }
+      break;
+    }
+
+  } while (choix != 5);
 
   return 0;
 }
