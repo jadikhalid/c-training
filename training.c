03d85abe967f3a222be940ec85cf06f4a1c6f8cd
Jadi Khalid <jadikhalid@gmail.com>
2026-04-27 09:34:36 +0100

    ok

--- training.c
+++ training.c
@@ -1,26 +1,37 @@
 /*
- * Exmemple d'utliosation de la fonction fgets()
+ * Nettoyage de stdin
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXLONG 10
+void clear_kb(void);
 
 int main()
 {
-  char buffer[MAXLONG];
-  puts("Entrez une ligne de texte à la fois, ou un blanc pour sortir.");
-  while (1)
-  {
-    fgets(buffer, MAXLONG, stdin);
-    if (buffer[0] == '\n')
-    {
-      break;
-    }
+  int age = -999;
+  char nom[20];
 
-    puts(buffer);
+  puts("Entrez votre âge :");
+  if (scanf("%u", &age) != 1)
+  {
+    puts("Erreur de saisie pour l'âge.");
   }
 
+  clear_kb();
+
+  puts("Entrez votre nom :");
+  scanf("%19s", nom);
+
+  printf("Vous avez %d ans\n", age);
+  printf("Vous vous appelez %s\n", nom);
+
   return 0;
+}
+
+void clear_kb(void)
+{
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
 }
