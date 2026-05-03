35e9ac209ee1aa2634911364d99c61c76fe3d605
jadikhalid <jadikhalid@gmail.com>
2026-05-03 13:41:42 +0100

    okiiii

--- training.c
+++ training.c
@@ -1,36 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int multi[2][4];
+
 int main()
 {
-  FILE *f = fopen("test.txt", "r");
-  if (f == NULL)
-  {
-    perror("Erreure d'ouverture de fichier");
-    return 1;
-  }
-  long long occurences[256] = {0};
-  int c;
+  printf("\nmulti = %p", (void *)multi);
+  printf("\nmulti[0] = %p", (void *)multi[0]);
+  printf("\nmulti[0][0] = %p", (void *)&multi[0][0]);
 
-  while ((c = fgetc(f)) != EOF)
-    occurences[(unsigned char)c]++;
-
-  fclose(f);
-
-  printf("Statistique des caracteres :\n");
-  for (int i = 0; i < 256; i++)
-  {
-    if (occurences[i] > 0)
-    {
-      // On affiche le caractère (si imprimable) et son nombre
-      if (i > 31 && i < 127)
-      {
-        printf("'%c' : %lld\n", i, occurences[i]);
-      }
-      else
-      {
-        printf("Code ASCII %d : %lld\n", i, occurences[i]);
-      }
-    }
-  }
+  return 0;
 }
