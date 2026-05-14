e331c53e958b352c162d378ee8d8e00a98572839
jadikhalid <jadikhalid@gmail.com>
2026-05-14 16:15:27 +0100

    Liste simplement chainee

--- training.c
+++ training.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-/*Prototype de fonctions */
-char fonction(char *tableau[]);
-char (*ptr)(char *[]) = fonction;
-
 int main()
 {
-  int tableau[2][3][4];
-  size_t taille = sizeof(tableau);
-
-  printf("%zu\n", taille);
+  char tableau[12][12];
+  char (*ptr)[12] = tableau;
+  for (int i = 0; i < 12; i++)
+  {
+    for (int j = 0; j < 12; j++)
+    {
+      *(*(ptr + i) + j) = 'X';
+      printf("%c ", *(*(ptr + i) + j));
+    }
+    printf("\n");
+  }
 
   return 0;
 }
