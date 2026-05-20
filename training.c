7465c23b37cb7470b84d2d6eacd04a2253cd6d45
jadikhalid <jadikhalid@gmail.com>
2026-05-20 09:36:51 +0100

    ...fseek()

--- training.c
+++ training.c
@@ -1,39 +1,70 @@
-/* Démonstration de ftell() et rewind() */
+/* Acces aleatoire avec fseek() */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFLEN 6
-
-char msg[] = "abcdefghijklmnopqrstuvwxyz";
+#define MAX 50
 
 int main()
 {
   FILE *fp;
-  char buf[BUFLEN];
+  int data, count, array[MAX];
+  long offset;
 
-  if ((fp = fopen("result.txt", "w")) == NULL)
+  /* Initialiser le tableau */
+  for (count = 0; count < MAX; count++)
+    array[count] = count * 2;
+
+  /* Ouvrir un fichier binaire en ecriture */
+  if ((fp = fopen("random.dat", "wb")) == NULL)
   {
-    fprintf(stderr, "Erreur à l'ouverture du fichier.");
+    fprintf(stderr, "\nErreur a l ouverture du fichier.");
     return 1;
   }
 
-  if (fputs(msg, fp) == EOF)
+  /* Ecrire le tableau dans le fichier puis le refermer */
+  if ((fwrite(array, sizeof(*array), MAX, fp)) != MAX)
   {
-    fprintf(stderr, "Erreur à l'écriture sur le fichier...");
+    fprintf(stderr, "\nErreur a l ecriture dans le fichier.");
     return 1;
   }
 
   fclose(fp);
-  fp = NULL;
 
-  /* Ouvrons maintenant le fichier en lecture */
-  if ((fp = fopen("result.txt", "r")) == NULL)
+  /* Ouvrir le fichier en lecture */
+  if ((fp = fopen("random.dat", "rb")) == NULL)
   {
-    fprintf(stderr, "Erreur lors de la lecture du fichier");
+    fprintf(stderr, "\nErreur a l ouverture du fichier");
     return 1;
   }
 
-  printf("\nImmédiattement aprés l'ouverture, la position du curseur dans le fichier %s  est : %ld", buf, ftell(fp));
+  /* Demander a l utilisateur quel element il veut lire
+  Lire l element et l afficher . Arreter lorsqu il
+  repond -1 */
 
-  /* Lire les 5 caractères suivants */
+  while (1)
+  {
+    printf("\nIndiquez l element a lire, 0-%d, \
+      -1 pour arreter : ",
+           MAX - 1);
+    scanf("%ld", &offset);
+    if (offset < 0)
+      break;
+    else if (offset > MAX - 1)
+      continue;
+    /* Deplacer l indicateur de position sur l element specifie */
+    if (fseek(fp, (offset * sizeof(int)), SEEK_SET))
+    {
+      fprintf(stderr, "\nErreur avec fseek().");
+      return 1;
+    }
+    /* Lire un unique entirt */
+    fread(&data, sizeof(data), 1, fp);
+
+    printf("\nL element %ld a la veleur %d.", offset, data);
+  }
+
+  fclose(fp);
+
+  return 0;
 }
