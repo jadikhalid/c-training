5d0b1f511b6b643bf6f6bb41a3683da9049f51de
jadikhalid <jadikhalid@gmail.com>
2026-05-25 11:05:47 +0100

    RAS

--- training.c
+++ training.c
@@ -1,37 +1,18 @@
-/* Demonstration de noms de fichier temporaires */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main()
 {
-  char *buffer;
-  int fd;
-  FILE *tmpfd;
-
-  /* Garnir le buffer avec un nom de fichier temporaire */
-
-  buffer = strdup("fichier_XXXXXX");
-
-  /* Crée le fichier temporaire */
-
-  if ((fd = mkstemp(buffer)) == -1)
+  FILE *fp;
+  if ((fp = fopen("resultat.txt", "r")) == NULL)
   {
-    fprintf(stderr, "Impossible de créer le fichier.\n");
-    return 1;
+    fprintf(stderr, "Erreur de la lecture du fichier");
+    return -1;
   }
-  if ((tmpfd = fdopen(fd, "wb")) == NULL)
-  {
-    fprintf(stderr, "Erreur lors de l ouverture du fichier");
-    return 1;
-  }
+  int c;
+  while ((c = fgetc(fp)) != EOF)
+    fputc(c, stdout);
 
-  /* Utiliser le fichier temporaire */
-  printf("Nom de fichier temporaire : %s", buffer);
-
-  fclose(tmpfd);
-  free(buffer);
+  fclose(fp);
 
   return 0;
 }
