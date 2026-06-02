d63724f5b90b11576c34a98f87b905566cc78d57
jadikhalid <jadikhalid@gmail.com>
2026-06-02 10:56:30 +0100

    Manipulation des chaines de caracteres...

--- training.c
+++ training.c
@@ -1,26 +1,21 @@
-/* Démonstration de strcpy() */
+/* La fonction strcat() */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char source[] = "Une chaine de caracteres.";
+char str1[] = "a";
+char str2[2];
 
 int main()
 {
-  char dest1[80];
-  char *dest2;
-
-  printf("\nsource : %s", source);
-
-  /* Copier cers dest1 */
-  strcpy(dest1, source);
-  printf("\ndest1 : %s", dest1);
-
-  /* Copier vers dest2 */
-  dest2 = malloc(strlen(source) + 1);
-  strcpy(dest2, source);
-  printf("\ndest2 : %s", dest2);
+  str2[1] = '\0';
+  for (int n = 98; n < 123; n++)
+  {
+    str2[0] = n;
+    strcat(str1, str2);
+    puts(str1);
+  }
 
   return 0;
 }
