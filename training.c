48c537bfdc88a4b4707cd1fba1326050e95141b8
jadikhalid <jadikhalid@gmail.com>
2026-06-03 09:45:25 +0100

    strtol/d/f...

--- training.c
+++ training.c
@@ -1,20 +1,38 @@
-/* La fonction strcat() */
-
+/* Démonstration de strtod() */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-char str1[] = "a";
-char str2[2];
+#include "utilitaires.h"
 
 int main()
 {
-  str2[1] = '\0';
-  for (int n = 98; n < 123; n++)
+  char buf[80];
+  double d;
+
+  while (1)
   {
-    str2[0] = n;
-    strcat(str1, str2);
-    puts(str1);
+    printf("\nTapez la chaine de caracteres a convertir"
+           "(Entrée pour terminer):");
+    lire_clavier(buf, sizeof(buf));
+
+    if (strlen(buf) == 0)
+    {
+      printf("Aucun caracteres de renseigné ! Bye !");
+      break;
+    }
+
+    errno = 0;
+    d = strtod(buf, NULL);
+
+    if (errno)
+      printf("Pas de valeur a convertir dans la chaine\n");
+    else
+    {
+      printf("Valeur convertie : %lf.", d);
+      break;
+    }
   }
 
   return 0;
