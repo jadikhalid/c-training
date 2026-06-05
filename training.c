465e71255e7e969c5ed5c355c7ee4e08d78ac603
jadikhalid <jadikhalid@gmail.com>
2026-06-05 11:00:52 +0100

    macros de test...

--- training.c
+++ training.c
@@ -1,39 +1,71 @@
-/* Démonstration de strtod() */
+/* Utilisation des macros de test de caracteres pour
+   réaliser  une fonction lisant un entier au clavier  */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-#include "utilitaires.h"
+int get_in(void);
 
 int main()
 {
-  char buf[80];
-  double d;
-
-  while (1)
-  {
-    printf("\nTapez la chaine de caracteres a convertir"
-           "(Entrée pour terminer):");
-    lire_clavier(buf, sizeof(buf));
-
-    if (strlen(buf) == 0)
-    {
-      printf("Aucun caracteres de renseigné ! Bye !");
-      break;
-    }
-
-    errno = 0;
-    d = strtod(buf, NULL);
-
-    if (errno)
-      printf("Pas de valeur a convertir dans la chaine\n");
-    else
-    {
-      printf("Valeur convertie : %lf.", d);
-      break;
-    }
-  }
+  int x;
+  x = get_in();
+  printf("Vous avez tape : %d.\n", x);
 
   return 0;
+}
+
+int get_in(void)
+{
+  int ch, i, sign = 1;
+  bool temoin = true;
+  while (temoin)
+  {
+    /* Ignorer les espaces en tete */
+    puts("Tapez un chiffre");
+    while (isspace(ch = getchar()))
+      ;
+    /* Si le premier caractere n'est pas numérique
+    le recracher et renvoyer 0 */
+    if (ch != '-' && ch != '+' && !isdigit(ch) && ch != EOF)
+    {
+      puts("Entree invalide");
+      /* VIDER LE STDIN : on consomme tout jusqu'au '\n' */
+      int c;
+      while ((c = getchar()) != '\n' && c != EOF)
+        ;
+      continue;
+    }
+
+    /* Si le premier caractere est un signe -,
+    placer le signe du résultat */
+    if (ch == '-')
+      sign = -1;
+
+    /* Si le premier caractere est un signe + ou
+    un signe -, lire le aractere suivant */
+    if (ch == '+' || ch == '-')
+      ch = getchar();
+
+    /* Lire les caractere jusqu a tomber sur un
+    non chiffre. Effectuer la conversion en
+    multipliant chacun des chiffres lus par
+    la bonne puissance de 10 */
+    for (i = 0; isdigit(ch); ch = getchar())
+      i = 10 * i + (ch - '0');
+
+    /* Corriger éventuellement le signe */
+    i *= sign;
+
+    /* Si on n a pas rencontré le EOF, on recrache le caractere
+    non numerique. */
+    if (ch != EOF)
+      ungetc(ch, stdin);
+
+    temoin = false;
+  }
+  /* Renvoyer la valeur finale*/
+  return i;
 }
