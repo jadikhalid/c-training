15cea9f047b364150b50bc5b00662128e25b73a8
jadikhalid <jadikhalid@gmail.com>
2026-06-08 10:19:06 +0100

    fonctions a nombre d arguments variable...

--- training.c
+++ training.c
@@ -1,64 +1,38 @@
+/* Focntions avec un nombre variables d'arguments */
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
-/**
- * Recherche la premiere occurrence de la chaine 'aiguille' dans 'meule_de_foin'
- * sans distinction entre majuscules et minuscules.
- *
- * @param meule_de_foin La chaine dans laquelle on cherche.
- * @param aiguille      La sous-chaine a rechercher.
- * @return Un pointeur sur le debut de la sous-chaine trouvee, ou NULL.
- */
-char *mon_strcasestr(const char *meule_de_foin, const char *aiguille)
+float moyenne(int num, ...);
+
+int main()
 {
-  // Regle standard de strstr : si l'aiguille est vide, on renvoie le debut de la meule
-  if (*aiguille == '\0')
-  {
-    return (char *)meule_de_foin;
-  }
+  float x;
+  x = moyenne(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  printf("\nLa premiere moyenne est %f.", x);
 
-  // Parcourir la meule de foin caractere par caractere
-  for (int i = 0; meule_de_foin[i] != '\0'; i++)
-  {
-
-    // Si le premier caractere correspond (en ignorant la casse)
-    if (tolower((unsigned char)meule_de_foin[i]) == tolower((unsigned char)aiguille[0]))
-    {
-
-      int j = 0;
-      // On verifie si toute la suite de l'aiguille correspond
-      while (aiguille[j] != '\0' && meule_de_foin[i + j] != '\0' &&
-             tolower((unsigned char)meule_de_foin[i + j]) == tolower((unsigned char)aiguille[j]))
-      {
-        j++;
-      }
-
-      // Si on a parcouru toute l'aiguille avec succes, on a trouve !
-      if (aiguille[j] == '\0')
-      {
-        return (char *)&meule_de_foin[i];
-      }
-    }
-  }
-
-  return NULL; // Aucune occurrence trouvee
-}
-
-int main(void)
-{
-  const char *texte = "Developper en Langage C est Genial";
-  const char *recherche = "lAnGaGe";
-
-  char *resultat = mon_strcasestr(texte, recherche);
-
-  if (resultat != NULL)
-  {
-    printf("Occurrence trouvee ! Reste de la chaine : \"%s\"\n", resultat);
-  }
-  else
-  {
-    printf("Sous-chaine non trouvee.\n");
-  }
+  x = moyenne(5, 121, 44, 57, 3, 66);
+  printf("\nLa deuxieme moyenne est %f.", x);
 
   return 0;
+}
+
+float moyenne(int num, ...)
+{
+  /* Déclarer une variable de type va_list */
+  va_list arg_ptr;
+  int count, total = 0;
+
+  /* Initialiser le pointeur vers les arguments */
+  va_start(arg_ptr, num);
+
+  /* Récupérer chaque argument de la liste des variables */
+  for (count = 0; count < num; count++)
+    total += va_arg(arg_ptr, int);
+
+  /* Donner un coup de nalai */
+  va_end(arg_ptr);
+
+  /* resultat */
+  return ((float)total / num);
 }
