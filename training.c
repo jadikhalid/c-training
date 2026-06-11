2f61be185a5086a8a22f1afc6ef66ef60055685f
jadikhalid <jadikhalid@gmail.com>
2026-06-11 11:13:19 +0100

    qsort() bsearch() continuation...

--- training.c
+++ training.c
@@ -1,30 +1,52 @@
-/* perror() et errno */
+/* Utilisation de qsort() et bsearch() pour des chaines de caracteres */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <string.h>
 
 #include "utilitaires.h"
 
+#define MAX 20
+
+int comp(const void *s1, const void *s2)
+{
+  return strcmp(*(char **)s1, *(char **)s2);
+}
+
 int main()
 {
-  FILE *fp;
-  char filename[80];
+  char *data[MAX], buf[80], *ptr, *key, **key1;
+  int count;
 
-  printf("Indiquez un nom de fichier : ");
-  lire_clavier(filename, sizeof(filename));
-
-  if ((fp = fopen(filename, "r")) == NULL)
+  /* Entrée d'une suite de mots */
+  printf("Tapez %d mots séparés par un appui sur Entrée ", MAX);
+  for (count = 0; count < MAX; count++)
   {
-    perror("l'ouverture du fichier a échoué !");
-    printf("errno : %d.\n", errno);
-    return EXIT_FAILURE;
+    printf("Mot %d : ", count + 1);
+    lire_clavier(buf, sizeof(buf));
+    data[count] = strdup(buf);
   }
+
+  /* Trier es mots - les pointeurs en fait */
+  qsort(data, MAX, sizeof(data[0]), comp);
+
+  /* Afficher les mots triés */
+  for (count = 0; count < MAX; count++)
+    printf("\n%d : %s", count + 1, data[count]);
+
+  /* Demander une clé de recherche */
+  printf("\n\nTapez une clé de recherche : ");
+  lire_clavier(buf, sizeof(buf));
+
+  /* Effectuer la recherche par clé */
+  key = buf;
+  key1 = &key;
+  ptr = bsearch(key1, data, MAX, sizeof(data[0]), comp);
+
+  if (ptr != NULL)
+    printf("%s trouvé.\n", buf);
   else
-  {
-    puts("Fichier ouvert en lecture");
-    fclose(fp);
-  }
+    printf("%s non trouvé.\n", buf);
 
-  return EXIT_SUCCESS;
+  exit(EXIT_SUCCESS);
 }
