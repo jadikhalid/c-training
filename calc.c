5282fc69276ccd6a849da3b1ec1b7bc52b4a9f67
jadikhalid <jadikhalid@gmail.com>
2026-06-15 10:07:05 +0100

    programmation modulaire...

---
+++ calc.c
@@ -0,0 +1,8 @@
+/* calcul du carré d'un integer */
+
+#include "calc.h"
+
+long sqr(int x)
+{
+    return x * x;
+}
--- training.c
+++ training.c
@@ -1,46 +1,14 @@
+/* Entrer un nombre et afficher son carré */
 #include <stdio.h>
-
-void afficher_binaire(unsigned int nombre)
-{
-  // Un unsigned int fait généralement 4 octets, soit 32 bits
-  int taille_bits = sizeof(nombre) * 8;
-  int bit_trouve = 0; // Pour ignorer les zéros inutiles au début (optionnel)
-
-  printf("Valeur binaire de %u : ", nombre);
-
-  // Si le nombre est strictement 0, on affiche juste 0
-  if (nombre == 0)
-  {
-    printf("0");
-  }
-  else
-  {
-    // On parcourt les bits du plus fort (gauche) au plus faible (droite)
-    for (int i = taille_bits - 1; i >= 0; i--)
-    {
-      // On décale le bit 'i' vers la position 0, et on applique un masque avec 1
-      int bit = (nombre >> i) & 1;
-
-      if (bit == 1)
-      {
-        bit_trouve = 1; // On a trouvé le premier bit significatif
-      }
-
-      // On commence à afficher seulement après le premier bit à 1
-      if (bit_trouve)
-      {
-        printf("%d", bit);
-      }
-    }
-  }
-  printf("\n");
-}
+#include <stdlib.h>
+#include "calc.h"
 
 int main()
 {
-  unsigned int valeur = 258;
-
-  afficher_binaire(valeur);
+  int x;
+  printf("Tapez un nombre entier : ");
+  scanf("%d", &x);
+  printf("\nLe carré de %d est %ld", x, sqr(x));
 
   return 0;
 }
