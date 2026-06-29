b3bcf8694755ed0168653cafb168029e1d8f8b15
kjadi <jadikhalid@gmail.com>
2026-06-29 09:25:39 +0100

    ...

--- training.c
+++ training.c
@@ -1,33 +1,53 @@
-#define _DEFAULT_SOURCE /* See feature_test_macros(7) */
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
 #include <grp.h>
+#include <stdint.h> // Nécessaire pour intmax_t
 
 int main(int argc, char *argv[])
 {
-  gid_t *table_gid = NULL;
   int i;
-  // int taille;
+  pid_t pid;
+  pid_t pgid;
 
-  if (argc < 2)
+  // Si aucun argument, on affiche les infos du processus courant
+  if (argc == 1)
   {
-    fprintf(stderr, "Usage %s GID ...\n", argv[0]);
-    exit(EXIT_FAILURE);
+    // On caste en intmax_t pour une compatibilité parfaite avec le format %jd
+    fprintf(stdout, "Processus courant : %jd | Groupe : %jd\n",
+            (intmax_t)getpid(), (intmax_t)getpgid(0));
+    return 0;
   }
-  if ((table_gid = calloc((size_t)(argc - 1), sizeof(gid_t))) == NULL)
-  {
-    fprintf(stderr, "Erreur calloc, errno = %d\n", errno);
-    exit(EXIT_FAILURE);
-  }
+
+  // Boucle de traitement des arguments
   for (i = 1; i < argc; i++)
-    if (sscanf(argv[i], "%u", &(table_gid[i - 1])) != 1)
+  {
+    // Utilisation de long pour stocker temporairement la lecture
+    long temp_pid;
+    if (sscanf(argv[i], "%ld", &temp_pid) != 1)
     {
-      fprintf(stderr, "GID invalide : %s\n", argv[i]);
-      exit(EXIT_FAILURE);
+      fprintf(stderr, "PID invalide : %s\n", argv[i]);
     }
+    else
+    {
+      pid = (pid_t)temp_pid;
+      pgid = getpgid(pid);
+
+      if (pgid == -1)
+      {
+        fprintf(stderr, "PID %jd inexistant ou erreur (errno: %d)\n",
+                (intmax_t)pid, errno);
+      }
+      else
+      {
+        fprintf(stdout, "PID : %jd | Groupe : %jd\n",
+                (intmax_t)pid, (intmax_t)pgid);
+      }
+    }
+  }
 
   return 0;
 }
