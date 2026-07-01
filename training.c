72d0f26bb8273845e541d7cfcd83055ca43d235d
kjadi <jadikhalid@gmail.com>
2026-07-01 10:38:35 +0100

    ok

--- training.c
+++ training.c
@@ -1,28 +1,12 @@
-#define _GNU_SOURCE
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
 
-int main(int argc, char *argv[])
+extern char **environ;
+
+int main(void)
 {
-  int i;
-  pid_t pid, sid;
-  if (argc == 1)
-  {
-    fprintf(stdout, "%d : %d\n", getpid(), getsid(0));
-    return 0;
-  }
-  for (i = 1; i < argc; i++)
-    if (sscanf(argv[i], "%d", &pid) != 1)
-      fprintf(stderr, "PID invalide : %s\n", argv[i]);
-    else
-    {
-      sid = getsid(pid);
-      if (sid == -1)
-        fprintf(stderr, "%d inexistant\n", pid);
-      else
-        fprintf(stdout, "%d : %d\n", pid, sid);
-    }
+  int i = 0;
+  for (i = 0; environ[i] != NULL; i++)
+    fprintf(stdout, "%d : %s\n", i, environ[i]);
 
   return 0;
 }
