a9773d642bd3cabf79ec75c05240a700f0eff4c6
kjadi <jadikhalid@gmail.com>
2026-07-07 09:58:44 +0100

    ok

--- training.c
+++ training.c
@@ -1,61 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <errno.h>
 
 int main(void)
 {
-  int pipefd[2];
-  char *user = getenv("USER");
+  FILE *sortie;
+  char ligne[128];
+  char etat[128];
 
-  if (user == NULL)
-    return 1;
-
-  // 1. Création d'un pipe pour communiquer avec le futur processus "mail"
-  if (pipe(pipefd) == -1)
+  if ((sortie = popen("/sbin/ifconfig eth0", "r")) == NULL)
   {
-    perror("pipe");
+    fprintf(stderr, " Erreur popen %d \n", errno);
     return 1;
   }
 
-  pid_t pid = fork();
-
-  if (pid == -1)
+  while (fgets(ligne, 127, sortie) != NULL)
   {
-    perror("fork");
-    return 1;
+    if (sscanf(ligne, "%s", etat) == 1)
+      if (strcmp(etat, "UP") == 0)
+      {
+        fprintf(stderr, "interface eth0 en marche \n");
+        pclose(sortie);
+        return 0;
+      }
   }
-
-  if (pid == 0)
-  {
-    // --- PROCESSUS FILS : va devenir "mail" ---
-    close(pipefd[1]);              // Ferme le côté écriture (non utilisé)
-    dup2(pipefd[0], STDIN_FILENO); // Redirige l'entrée standard vers le pipe
-    close(pipefd[0]);
-
-    // Exécution directe de "mail" sans passer par un shell
-    // execlp cherche "mail" dans le PATH
-    execlp("mail", "mail", user, NULL);
-
-    // Si execlp échoue
-    perror("execlp");
-    exit(1);
-  }
-  else
-  {
-    // --- PROCESSUS PÈRE : envoie le message ---
-    close(pipefd[0]); // Ferme le côté lecture (non utilisé)
-
-    FILE *message = fdopen(pipefd[1], "w");
-    if (message != NULL)
-    {
-      fprintf(message, "Ceci est un message sécurisé\n");
-      fprintf(message, "écrit par moi-même sans shell\n");
-      fclose(message); // Ferme et envoie le signal EOF au processus mail
-    }
-
-    wait(NULL); // Attend la fin du fils
-  }
-
+  fprintf(stdout, "interface eth0 inactive \n");
+  pclose(sortie);
   return 0;
 }
