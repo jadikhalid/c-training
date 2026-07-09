ac2f8e66038351924294b049fd26d0bead08a137
kjadi <jadikhalid@gmail.com>
2026-07-09 10:34:55 +0100

    pas de retour en arriere

--- training.c
+++ training.c
@@ -2,25 +2,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
 #include <sys/wait.h>
+#include <sys/resource.h>
 
 int main(void)
 {
   pid_t pid;
   int status;
+  struct rusage usage;
+
   switch (pid = fork())
-  {
-  case -1:
-    fprintf(stderr, "Erreur dans fork()\n");
-    exit(1);
-  case 0:
-    fprintf(stdout, "Fils 1 : PID = %u\n", getpid());
-    while (1)
-      pause();
-  default:
-    break;
-  }
-  switch (fork())
   {
   case -1:
     fprintf(stderr, "Erreur dans fork()\n");
