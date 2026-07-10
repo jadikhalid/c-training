cdcf0c4dc9d3bc7ff35f1915edea714a32f1334d
kjadi <jadikhalid@gmail.com>
2026-07-10 11:17:55 +0100

    ok

--- training.c
+++ training.c
@@ -1,21 +1,14 @@
+#define _GNU_SOURCE
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
 #include <errno.h>
-#include <sys/wait.h>
-#include <sys/resource.h>
 
 int main(void)
 {
-  pid_t pid;
-  int status;
-  struct rusage usage;
-
-  switch (pid = fork())
-  {
-  case -1:
-    fprintf(stderr, "Erreur dans fork()\n");
-    exit(1);
-  }
+  int i;
+  for (i = 0; i < sys_nerr; i++)
+    if (sys_errlist[i] != NULL)
+      fprintf(stdout, "%d : %s\n", i, sys_errlist[i]);
+    else
+      fprintf(stdout, "** Pas de message pour %d **\n", i);
+  return 0;
 }
