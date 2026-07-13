7a28f98f9da8a2c6605e2f8e9fc00e8e85014078
kjadi <jadikhalid@gmail.com>
2026-07-13 10:20:04 +0100

    ok

--- training.c
+++ training.c
@@ -1,15 +1,23 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 #include <string.h>
+
+void gestionnaire(int numero_signal)
+{
+  fprintf(stdout, "\n %u a recu le signal %d (%s)\n", getpid(), numero_signal, strsignal(numero_signal));
+}
 
 int main(void)
 {
-  int i;
-  fprintf(stdout, "strsignal() :\n");
-  for (i = 1; i < NSIG; i++)
-    fprintf(stdout, "%d : %s\n", i, strsignal(i));
+  for (int i = 1; i < _NSIG; i++)
+    if (signal(i, gestionnaire) == SIG_ERR)
+      fprintf(stderr, "Signal %d non capturé \n", i);
+  while (1)
+  {
+    pause();
+  }
 
   return 0;
 }
