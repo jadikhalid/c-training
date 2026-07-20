6296fbcc39fc44ae74781c60ef5bac7fad782dec
kjadi <jadikhalid@gmail.com>
2026-07-20 11:06:25 +0100

    les timer ...

--- training.c
+++ training.c
@@ -1,48 +1,44 @@
 #define _POSIX_C_SOURCE 200809L
-#define _GNU_SOURCE
 
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <errno.h>
 
-void syntaxe(const char *nom)
+static int temporisation_ecoulee;
+
+void gestionnaie_sigalrm(int inutile)
 {
-  fprintf(stderr, "syntaxe %s signal pid ...\n", nom);
-  exit(1);
+  (void)inutile;
+  temporisation_ecoulee = 1;
 }
 
-int main(int argc, char *argv[])
+int sommeil_precis(long nb_secondes, long nb_micosecondes)
 {
-  int i;
-  int numero;
-  pid_t pid;
-  union sigval valeur;
+  struct sigaction action;
+  struct sigaction ancienne_action;
+  sigset_t masque_sigalrm;
+  sigset_t ancien_masque;
 
-  if (argc == 1)
-    syntaxe(argv[0]);
+  int sigalrm_dans_ancien_mask = 0;
 
-  i = 1;
-  if (argc == 2)
-    numero = SIGTERM;
-  else
-  {
-    if (sscanf(argv[i], "%d", &numero) != 1)
-      syntaxe(argv[0]);
-    i++;
-  }
-  if ((numero < 0) || (numero > NSIG - 1))
-    syntaxe(argv[0]);
-  valeur.sival_int = 0;
-  for (; i < argc; i++)
-  {
-    if (sscanf(argv[i], "%d", &pid) != 1)
-      syntaxe(argv[0]);
-    if (sigqueue(pid, numero, valeur) < 0)
-    {
-      fprintf(stderr, "%u", pid);
-      perror("");
-    }
-  }
-  return 0;
+  struct itimerval ancien_timer;
+  struct itimerval nouveau_timer;
+
+  int retour = 0;
+
+  /* Préparation du timer */
+  timeclear(&(nouveau_timer.it_interval));
+  nouveau_timer.it_value.tv_sec = nb_secondes;
+  nouveau_timer.it_value.tv_usec = nb_micosecondes;
+
+  /* Installation du gestionnaire d'alarme */
+  action.sa_handler = gestionnaie_sigalrm;
+  sigemptyset(&(action.sa_mask));
+  action.sa_flags = SA_RESTART;
+  if (sigprocmask(SIGALRM, &action, &ancienne_action) != 0)
+    return (-1);
 }
