b2510c036aacc2d7c71989aab2df5ad09d2839c8
kjadi <jadikhalid@gmail.com>
2026-07-21 10:11:03 +0100

    C Timer Management

--- training.c
+++ training.c
@@ -8,37 +8,38 @@
 #include <sys/time.h>
 #include <errno.h>
 
-static int temporisation_ecoulee;
+unsigned long int mode_utilisateur;
+unsigned long int mode_utilisateur_et_noyau;
 
-void gestionnaie_sigalrm(int inutile)
-{
-  (void)inutile;
-  temporisation_ecoulee = 1;
-}
+void gestionnaire_signaux(int numero);
+void fin_du_suivi(void);
+void action_a_mesurer(void);
 
-int sommeil_precis(long nb_secondes, long nb_micosecondes)
+int main(void)
 {
   struct sigaction action;
-  struct sigaction ancienne_action;
-  sigset_t masque_sigalrm;
-  sigset_t ancien_masque;
+  struct itimerval timer;
 
-  int sigalrm_dans_ancien_mask = 0;
+  /* Preparation du timer */
+  timer.it_value.tv_sec = 0;
+  timer.it_value.tv_usec = 10000;
+  timer.it_interval.tv_sec = 0;
+  timer.it_interval.tv_usec = 10000;
 
-  struct itimerval ancien_timer;
-  struct itimerval nouveau_timer;
-
-  int retour = 0;
-
-  /* Préparation du timer */
-  timeclear(&(nouveau_timer.it_interval));
-  nouveau_timer.it_value.tv_sec = nb_secondes;
-  nouveau_timer.it_value.tv_usec = nb_micosecondes;
-
-  /* Installation du gestionnaire d'alarme */
-  action.sa_handler = gestionnaie_sigalrm;
+  /* Installation du gestionnaire de signaux */
+  action.sa_handler = gestionnaire_signaux;
   sigemptyset(&(action.sa_mask));
   action.sa_flags = SA_RESTART;
-  if (sigprocmask(SIGALRM, &action, &ancienne_action) != 0)
+  if ((sigaction(SIGVTALRM, &action, NULL) != 0) || (sigaction(SIGPROF, &action, NULL) != 0))
+  {
+    fprintf(stderr, "Erreur á l'appel de la fonction sigaction()");
     return (-1);
+  }
+
+  /* Déclenchement des nouveaux timers */
+  if ((setitimer(ITIMER_VIRTUAL, &timer, NULL)) != 0 || (setitimer(ITIMER_PROF, &timer, NULL)) != 0)
+  {
+    fprintf(stderr, "Erreur á l'appel de setitmer()");
+    return -1;
+  }
 }
