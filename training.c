b226b300ddc205408b66a5413fd49ea519a59ef3
kjadi <jadikhalid@gmail.com>
2026-07-31 10:50:52 +0100

    Pthreads_functions....

--- training.c
+++ training.c
@@ -1,27 +1,45 @@
-#define _GNU_SOURCE
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
 
-#define NB_THREADS 5
-
-void *fn_thread(void *numero);
-
-static int compteur = 0;
+void *fn_thread(void *inutile)
+{
+  (void)inutile;
+  char chaine[128];
+  int i = 0;
+  fprintf(stdout, "Thread : entrez un nombre :");
+  while (fgets(chaine, 128, stdin) != NULL)
+    if (sscanf(chaine, "%d", &i) != 1)
+      fprintf(stdout, "Un nombre SVP :");
+    else
+      break;
+  pthread_exit((void *)(intptr_t)i);
+}
 
 int main(void)
 {
-  pthread_t thread[NB_THREADS];
   int i;
   int ret;
+  void *retour;
+  pthread_t thread;
 
-  for (i = 0; i < NB_THREADS; i++)
-    if ((ret = pthread_create(&thread[i], NULL, fn_thread, (void *)i)) != 0)
-    {
-      fprintf(stderr, "%s", strerror(ret));
-      exit(1);
-    }
+  if ((ret = pthread_create(&thread, NULL, fn_thread, NULL)) != 0)
+  {
+    perror("Erreur lors de la creation du thread");
+    exit(1);
+  }
+
+  pthread_join(thread, &retour);
+
+  if (retour != PTHREAD_CANCELED)
+  {
+    i = (int)(intptr_t)retour;
+    fprintf(stdout, "main : valeur lue = %d\n", i);
+  }
+
+  return 0;
 }
