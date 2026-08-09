31f5dd333197b7fe953a8534b2ec5a425664eff0
kjadi <jadikhalid@gmail.com>
2026-08-09 10:27:30 +0100

    projection with mmap and munmap...

--- training.c
+++ training.c
@@ -10,18 +10,41 @@ int main(int argc, char *argv[])
     char *projection;
     int fichier;
     struct stat etat_fichier;
-    long taille_fichier;
-    int i;
+    size_t i, taille_fichier;
     char tmp;
 
     if (argc != 2)
     {
         fprintf(stderr, "Syntaxe : %s fichier á inverser\n", argv[0]);
-        ext(1);
+        exit(1);
     }
-    if ((fichier = open(arg[1], O_RDWR)) < 0)
+    if ((fichier = open(argv[1], O_RDWR)) < 0)
     {
         perror("Erreure á l'ouverture du fichier");
         exit(1);
     }
+    if (stat(argv[1], &etat_fichier) != 0)
+    {
+        perror("stat");
+        exit(1);
+    }
+
+    taille_fichier = (size_t)etat_fichier.st_size;
+    projection = (char *)mmap(NULL, taille_fichier, PROT_READ | PROT_WRITE, MAP_SHARED, fichier, 0);
+    if (projection == (char *)MAP_FAILED)
+    {
+        perror("mmap");
+        exit(1);
+    }
+    close(fichier);
+
+    for (i = 0; i < taille_fichier / 2; i++)
+    {
+        tmp = projection[i];
+        projection[i] = projection[taille_fichier - i - 1];
+        projection[taille_fichier - i - 1] = tmp;
+    }
+    munmap((void *)projection, taille_fichier);
+
+    return 0;
 }
