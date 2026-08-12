d060944b514cb0a495a1b24e687abcdddf48de3c
kjadi <jadikhalid@gmail.com>
2026-08-12 11:10:45 +0100

    Manipulation des blocs mémoire...

--- training.c
+++ training.c
@@ -1,52 +1,29 @@
-#define _GNU_SOURCE
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
 #include <string.h>
 
-#define TAILLE_CHAINE 128
-
-void *mon_malloc_avec_mmap(size_t taille)
+void affiche_resultats(unsigned char *bloc_1, unsigned char *bloc_2, unsigned int lg)
 {
-    void *retour;
-    retour = mmap(NULL, taille, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
-    if (retour == MAP_FAILED)
-    {
-        perror("mmap");
-        return NULL;
-    }
-    return retour;
+    unsigned int i;
+    fprintf(stdout, "bloc_1 = ");
+    for (i = 0; i < lg; i++)
+        fprintf(stdout, "%02d ", bloc_1[i]);
+    fprintf(stdout, "\n");
+    fprintf(stdout, "bloc_2 = ");
+    for (i = 0; i < lg; i++)
+        fprintf(stdout, "%02d ", bloc_2[i]);
+    fprintf(stdout, "\n");
+    fprintf(stdout, "memcmp (bloc1, bloc2, %d)= %d\n", lg, memcmp(bloc_1, bloc_2, lg));
+    fprintf(stdout, "\n");
 }
 
 int main(void)
 {
-    char *chaine = NULL;
-    fprintf(stdout, "Allocation de %d octets \n", TAILLE_CHAINE);
-    chaine = mon_malloc_avec_mmap(TAILLE_CHAINE);
-    if (chaine == NULL)
-    {
-        perror("mmap");
-        exit(1);
-    }
-    fprintf(stdout, "Protections par défaut \n");
-    fprintf(stdout, "Ecriture...");
-    strcpy(chaine, "ok");
-    fprintf(stdout, "ok\n");
-    fprintf(stdout, "Lecture...");
-    fprintf(stdout, "%s\n", chaine);
-    fprintf(stdout, "Interdiction d'écriture \n");
-
-    if (mprotect(chaine, TAILLE_CHAINE, PROT_READ) < 0)
-    {
-        perror("mprotect");
-        exit(1);
-    }
-
-    fprintf(stdout, "Lecture ...\n");
-    fprintf(stdout, "%s\n", chaine);
-    fprintf(stdout, "Ecritue...");
-    strcpy(chaine, "Non");
+    unsigned char bloc_1[4] = {0x01, 0x02, 0x03, 0x04};
+    unsigned char bloc_2[4] = {0x01, 0x02, 0x08, 0x04};
+    unsigned char bloc_3[4] = {0x01, 0x00, 0x03, 0x04};
+    affiche_resultats(bloc_1, bloc_1, 4);
+    affiche_resultats(bloc_1, bloc_2, 4);
+    affiche_resultats(bloc_1, bloc_3, 4);
 
     return 0;
 }
