4b171fd26442730fdc43a49b079bf3aa69ab38f3
kjadi <jadikhalid@gmail.com>
2026-08-21 09:55:41 +0100

    twalk...

--- training.c
+++ training.c
@@ -1,76 +1,90 @@
+#include <search.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void *b_insert(const void *cle, const void *table, size_t *nb_elements, size_t taille_element, int (*compare)(const void *lm1, const void *lm2))
-{
-    const void *element;
-    int comparaison;
-    size_t bas = 0;
-    size_t haut = (*nb_elements);
-    size_t milieu;
-
-    while (bas < haut)
-    {
-        milieu = (bas + haut) / 2;
-        element = (void *)(((const char *)table) + (milieu * taille_element));
-        comparaison = compare(cle, element);
-        if (comparaison < 0)
-            haut = milieu;
-        else if (comparaison > 0)
-            bas = milieu + 1;
-        else
-            return ((void *)element);
-    }
-
-    if (haut >= (*nb_elements))
-    {
-        element = (void *)(((const char *)table) + ((*nb_elements) * taille_element));
-    }
-    else
-    {
-        element = (void *)(((const char *)table) + (haut * taille_element));
-        if (compare(cle, element) > 0)
-        {
-            element = (const char *)element + taille_element;
-            haut++;
-        }
-        memmove((char *)element + taille_element, (void *)element, ((*nb_elements) - haut) * taille_element);
-    }
-    memcpy((void *)element, cle, taille_element);
-    (*nb_elements)++;
-
-    return ((void *)element);
-}
-
 int compare_char(const void *lm1, const void *lm2)
 {
-    return (((char *)lm1)[0] - ((char *)lm2)[0]);
+    return (strcmp(lm1, lm2));
 }
 
-int main(int argc, char *argv[])
+static VISIT type_parcours;
+
+void parcours(const void *noeud, const VISIT methode, const int profondeur)
 {
-    char *table = NULL;
-    size_t longueur;
-    if (argc != 3)
-    {
-        fprintf(stderr, "Syntaxe : %s table_element\n", argv[0]);
-        exit(1);
-    }
-    longueur = strlen(argv[1]);
-    if ((table = (char *)malloc((size_t)(longueur + 2))) == NULL)
-    {
-        perror("malloc");
-        exit(1);
-    }
-    strcpy(table, argv[1]);
-    fprintf(stdout, "tri avec qsort ...\n");
-    qsort(table, strlen(table), 1, compare_char);
-    fprintf(stdout, "%s\n", table);
-    fprintf(stdout, "recherche / insertion de %c\n", argv[2][0]);
-    b_insert((void *)argv[2], table, &longueur, 1, compare_char);
-    table[longueur] = '\0';
-    fprintf(stdout, "%s\n", table);
+    (void)profondeur;
+    if (methode == type_parcours)
+        fprintf(stdout, "%s ", *(char **)noeud);
+    else if (methode == leaf)
+        fprintf(stdout, "(%s) ", *(char **)noeud);
+}
+
+int main(void)
+{
+    int i;
+    void *racine = NULL;
+    char *chaines[] = {
+        "A",
+        "Z",
+        "E",
+        "R",
+        "T",
+        "Y",
+        "U",
+        "I",
+        "O",
+        "P",
+        "Q",
+        "S",
+        "D",
+        "F",
+        "G",
+        "H",
+        "J",
+        "K",
+        "L",
+        "M",
+        "W",
+        "X",
+        "C",
+        "V",
+        "B"
+        "N",
+        NULL,
+    };
+    /* Insertion des chaines dans l'arbre binaire */
+    for (i = 0; chaines[i] != NULL; i++)
+        if (tsearch(chaines[i], &racine, compare_char) == NULL)
+        {
+            perror("tsearch");
+            exit(1);
+        }
+    for (i = 0; chaines[i] != NULL; i++)
+        if (tfind(chaines[i], &racine, compare_char) == NULL)
+        {
+            fprintf(stderr, "%s perdue ?\n", chaines[i]);
+            exit(1);
+        }
+    fprintf(stdout, "Parcours preorder (+leaf) : \n");
+    type_parcours = preorder;
+    twalk(racine, parcours);
+
+    fprintf(stdout, "\n");
+
+    fprintf(stdout, "Parcours postorder (+leaf) : \n ");
+    type_parcours = postorder;
+    twalk(racine, parcours);
+    fprintf(stdout, "\n");
+
+    fprintf(stdout, "Pacours endorder (+leaf) : \n");
+    type_parcours = endorder;
+    twalk(racine, parcours);
+    fprintf(stdout, "\n");
+
+    fprintf(stdout, "Parcours Leaf : \n");
+    type_parcours = leaf;
+    twalk(racine, parcours);
+    fprintf(stdout, "\n");
 
     return 0;
 }
