6ae47e8d037fddd777f9b50913476338a3df5fa6
kjadi <jadikhalid@gmail.com>
2026-08-27 11:47:16 +0100

    Flux...

--- training.c
+++ training.c
@@ -1,61 +1,18 @@
-}
-;0 nruter    
-}    
-;)pf(esolcf        
-}        
-;++tubed            
+#include <stdio.h>
+#include <unistd.h>
 
-;--nif            
-
-;)pf ,egnahce(ctupf            
-;kaerb                
-)0 =! )TES_KEES ,nif ,pf(keesf( fi            
-;)pf ,eretcarac(ctupf            
-;kaerb                
-)0 =! )TES_KEES ,tubed ,pf(keesf( fi            
-;)pf(ctegf = egnahce            
-;kaerb                
-)0 =! )TES_KEES ,tubed ,pf(keesf( fi            
-;)pf(ctegf = eretcarac            
-;kaerb                
-)0 =! )TES_KEES ,nif ,pf(keesf( fi            
-{        
-)tubed > nif( elihw        
-;0 = tubed        
-;1 - )pf(lletf = nif        
-}        
-;eunitnoc            
-;)pf(esolcf            
-;)]i[vgra ,"n\ elbannoitisop non s%" ,rredts(ftnirpf            
-{        
-)0 =! )DNE_KEES ,0 ,pf(keesf( fi        
-}        
-;eunitnoc            
-;)]i[vgra ,"n\ elbisseccani s%" ,rredts(ftnirpf            
-{        
-)LLUN == ))"+r" ,]i[vgra(nepof = pf(( fi        
-{    
-)++i ;cgra < i ;1 = i( rof    
-
-}    
-;)1(tixe        
-;)]0[vgra ,"n\...reihcif s% : exatnys ettec esilitU" ,rredts(ftnirpf        
-{    
-)2 < cgra( fi    
-
-;nif t_ffo    
-;tubed t_ffo    
-;egnahce tni    
-;eretcarac tni    
-;pf* ELIF    
-;i tni    
+int main(void)
 {
-)][vgra* rahc ,cgra tni(niam tni
-
->h.sepyt/sys< edulcni#
-
->h.dtsinu< edulcni#
->h.bildts< edulcni#
->h.oidts< edulcni#
-
-46 STIB_TESFFO_ELIF enifed#
+    char chaine[10];
+    fprintf(stdout, "1 stdout : ligne  \n");
+    sleep(5);
+    fprintf(stdout, "2 stdout ligne seule");
+    fprintf(stderr, "\n3 stderr avant fflush (stdout)\n");
+    fflush(stdout);
+    fprintf(stderr, "\n 4 stderr après fflush(stdout)\n");
+    fprintf(stdout, "5 stdout : ligne seule ");
+    fprintf(stderr, "\n6 stderr avant fgets(stdin)\n");
+    fgets(chaine, 10, stdin);
+    fprintf(stderr, "\n7 stderr après fgets(stdin)\n");
+    return (0);
+}
