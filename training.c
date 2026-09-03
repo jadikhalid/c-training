3bf5cc1ea2508549518a73da28bbed6fd9189a43
kjadi <jadikhalid@gmail.com>
2026-09-03 10:45:09 +0100

    fd/FILE/fcntl...

--- training.c
+++ training.c
@@ -1,59 +1,24 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 
 int main(void)
 {
-    int fd;
-    pid_t pid_fils;
-    off_t position;
+    int etat;
+    etat = fcntl(STDIN_FILENO, F_GETFL) & O_ACCMODE;
+    fprintf(stderr, "stdin : %s\n", (etat == O_RDWR) ? "R/W" : (etat == O_RDONLY) ? "R"
+                                                                                  : "W");
+    etat = fcntl(STDOUT_FILENO, F_GETFL) & O_ACCMODE;
+    fprintf(stderr, "stdout : %s\n", (etat == O_RDWR) ? "R/W" : (etat == O_RDONLY) ? "R"
+                                                                                   : "W");
 
-    fd = open("essai.lseek", O_RDWR | O_CREAT | O_TRUNC, 0644);
-
-    if (fd < 0)
-    {
-        perror("open");
-        exit(1);
-    }
-
-    if (write(fd, "ABCDEFGHIJ", 10) != 10)
-    {
-        perror("write");
-        exit(1);
-    }
-
-    if ((pid_fils = fork()) < 0)
-    {
-        perror("fork");
-        exit(1);
-    }
-    if (pid_fils)
-    {
-        position = lseek(fd, 0, SEEK_CUR);
-        fprintf(stderr, "Pere : position = %ld\n", position);
-        sleep(1);
-        position = lseek(fd, 0, SEEK_CUR);
-        fprintf(stderr, "Pere : position = %ld\n", position);
-        lseek(fd, 5, SEEK_SET);
-        fprintf(stderr, "Pere : déplacement en position 5\n");
-
-        wait(NULL);
-    }
-    else
-    {
-        position = lseek(fd, 0, SEEK_CUR);
-        fprintf(stderr, "Fils : position = %ld \n", position);
-        lseek(fd, 2, SEEK_SET);
-        fprintf(stderr, "Fils : decalement de 2 positions depuis le dédut du fichier\n");
-        sleep(2);
-        position = lseek(fd, 0, SEEK_CUR);
-        fprintf(stderr, "Fils : position = %ld \n", position);
-    }
-    close(fd);
+    etat = fcntl(STDERR_FILENO, F_GETFL) & O_ACCMODE;
+    fprintf(stderr, "stderr : %s\n", (etat == O_RDWR) ? "R/W" : (etat == O_RDONLY) ? "R"
+                                                                                   : "W");
 
     return 0;
 }
